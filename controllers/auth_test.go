a09c748943fcc3cb62eea8736c6e275d0654786c
Zulfikar Fajri <zulfikar.f@spacetoongo.com>
2023-10-07 23:31:18 +0700

    refacator: init refactor for register user test

--- controllers/auth_test.go
+++ controllers/auth_test.go
@@ -15,21 +15,40 @@ type ResponseModel struct {
 	User    models.User         `json:"user"`
 }
 
-func TestCreateUserReturnsCreatedStatus(t *testing.T) {
-	newUser := models.SignupInput{
-		Username: "johndoe",
-		Email:    "john@doe.com",
-		Password: "password",
-	}
+func TestRegisterUser(t *testing.T) {
+	t.Run("Should return 201 and confirmed valid input", func(t *testing.T) {
+		newUser := models.SignupInput{
+			Username: "johndoe",
+			Email:    "john@doe.com",
+			Password: "password",
+		}
 
-	writer := makeRequest("POST", "/register", newUser, false)
+		writer := makeRequest("POST", "/register", newUser, false)
 
-	var result ResponseModel
-	json.Unmarshal(writer.Body.Bytes(), &result)
+		var result ResponseModel
+		json.Unmarshal(writer.Body.Bytes(), &result)
 
-	assert.Equal(t, http.StatusCreated, writer.Code)
-	assert.Equal(t, "User created!", result.Message)
-
-	assert.NotZero(t, result.User.ID, "User ID should not be 0")
-	assert.False(t, result.User.CreatedAt.IsZero(), "CreatedAt should not be the zero timestamp")
+		assert.Equal(t, http.StatusCreated, writer.Code)
+		assert.Equal(t, "User created!", result.Message)
+		assert.Equal(t, 3, len(result.Errors))
+	})
 }
+
+// func TestCreateUserReturnsCreatedStatus(t *testing.T) {
+// 	newUser := models.SignupInput{
+// 		Username: "johndoe",
+// 		Email:    "john@doe.com",
+// 		Password: "password",
+// 	}
+
+// 	writer := makeRequest("POST", "/register", newUser, false)
+
+// 	var result ResponseModel
+// 	json.Unmarshal(writer.Body.Bytes(), &result)
+
+// 	assert.Equal(t, http.StatusCreated, writer.Code)
+// 	assert.Equal(t, "User created!", result.Message)
+
+// 	assert.NotZero(t, result.User.ID, "User ID should not be 0")
+// 	assert.False(t, result.User.CreatedAt.IsZero(), "CreatedAt should not be the zero timestamp")
+// }
